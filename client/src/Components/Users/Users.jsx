a78c31372b6a76c5c4f3d9bc079f241d1a2da0b4
Jonas Olofsson <jonas.olofsson81@hotmail.com>
2023-08-15 15:01:25 +0200

    Co-authored-by: Ammar Hasan <ammarhasan011@users.noreply.github.com>
    a little bit on the way

--- client/src/Components/Users/Users.jsx
+++ client/src/Components/Users/Users.jsx
@@ -1,25 +1,43 @@
-import { useEffect} from 'react'
+import { useEffect, useState} from 'react'
 import { io } from "socket.io-client";
 import './Users.css'
+import Lobby from '../Lobby/Lobby';
 
 export const Users = () => {
+  const [username, setUsername] = useState('')
+
+  
+
   //Calling server
   const socket = io("http://localhost:3000/", { autoConnect: false });
 
 const start_chat_with_user = () => {
-  socket.connect();
+  socket.connect()
+  console.log(username)
+  // if(username.trim() !== '') {
+  // console.log("Startar chatt med användare", username);
+   
+  // }else {
+  //   console.log("Användarnamnet är inte tillåtet")
+  // }
+  
 }
 
-  useEffect(() => {
-    socket.on("new_user", (username) => {
-      console.log(username);
-    });
-  }, []);
+  // useEffect(() => {
+  //   socket.on("new_user", (username) => {
+  //     console.log(username);
+  //   });
+  // }, []);
+
+
 
   return (
     <>
-      <input type="text" placeholder="Namn"></input>
+      <input type="text" placeholder="Namn" onChange={(e) => {setUsername(e.target.value)}}></input>
       <button onClick={start_chat_with_user}>Börja chatta</button>
+   
+      <Lobby socket={socket} username={username}/>
+    
     </>
   );
 };
