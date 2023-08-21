fa5da675b10794b595f35565aff3a9ffc7d1f0a1
Ammar Hasan <ammarhasan011@gmail.com>
2023-08-21 09:33:35 +0200

    Join selected room

--- client/src/Components/ChatPage/ChatPage.jsx
+++ client/src/Components/ChatPage/ChatPage.jsx
@@ -4,133 +4,155 @@ import { useSocket } from "../../Context/ContextForSocket";
 
 import Users from "../Users/Users";
 
-
 const ChatPage = ({ newUsername, room }) => {
   const socket = useSocket(); //using socket from context!
 
-
-  const [ newRoom, setNewroom ] = useState('Lobbyn')
-  const [ roomList, setRoomlist ] = useState(['Lobbyn'])
-  const [ currentRoom, setCurrentRoom ] = useState('Lobbyn')
-  const [ currentMessage, setCurrentMessage ] = useState('')
-  const [ messageList, setMessageList ] = useState([])
-  const [ leaveChat, setLeaveChat ] = useState(false)
-  console.log(roomList)
-  console.log(currentRoom)
+  const [newRoom, setNewroom] = useState("Lobbyn");
+  const [roomList, setRoomlist] = useState(["Lobbyn"]);
+  const [currentRoom, setCurrentRoom] = useState("Lobbyn");
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+  const [leaveChat, setLeaveChat] = useState(false);
+  const [selectedRoom, setSelectedRoom] = useState("Lobbyn");
+  console.log(roomList);
+  console.log(currentRoom);
 
   const sendMessage = async () => {
-    if (currentMessage !== '') {
+    if (currentMessage !== "") {
       const messageData = {
         room: newRoom,
         author: newUsername,
         msg: currentMessage,
-           time:
-          new Date()
-      }
+        time: new Date(),
+      };
 
-      await socket.emit('send_message', messageData);
-      console.log(messageData)
+      await socket.emit("send_message", messageData);
+      console.log(messageData);
       //setMessageList((list) => [...list, messageData])
     }
-  }
+  };
 
- 
   //listens for rooms-list updates from server
   useEffect(() => {
-    socket.on('roomList', (rooms) => {
-      setRoomlist(rooms)
-    })
-  }, [])
-
+    socket.on("roomList", (rooms) => {
+      setRoomlist(rooms);
+    });
+  }, []);
 
   useEffect(() => {
-      socket.on('receive_message', (data) => {
-       setMessageList((list) => [...list, data])
-
-      })
-  }, [socket])
-
-
+    socket.on("receive_message", (data) => {
+      setMessageList((list) => [...list, data]);
+    });
+  }, [socket]);
 
   const checkRoomInput = () => {
-    console.log(newRoom)
+    console.log(newRoom);
     if (newRoom.trim() != "") {
       //sending username and room to the server(terminal).
       socket.emit("start_chat_with_room", newRoom);
-      console.log(newRoom)
-      setCurrentRoom(newRoom)
+      console.log(newRoom);
+      setCurrentRoom(newRoom);
     } else {
       alert("Fältet får inte vara tomt.");
     }
   };
 
+  // const leaveRoom = () => {
+  //   console.log("Left chat");
+  //   socket.disconnect();
+  //   console.log("Socket disconnected:", socket.disconnected); //boolean proves cocket`s disconnect
+  //   setLeaveChat(true); //updating state
+  //   // setSelectedRoom("lobbyn");
+  // };
   const leaveRoom = () => {
-    console.log('Left chat')
-    socket.disconnect()
-    console.log('Socket disconnected:', socket.disconnected) //boolean proves cocket`s disconnect
-    setLeaveChat(true) //updating state
-  }
-  
+    socket.emit(newUsername, "Left chat", currentRoom);
+    setSelectedRoom("lobbyn");
+    setCurrentRoom("Lobbyn");
+    setLeaveChat(true); //updating state
+    setMessageList([]);
+  };
+
+  const joinSelectedRoom = () => {
+    if (selectedRoom.trim() != "") {
+      socket.emit("start_chat_with_room", selectedRoom);
+      setCurrentRoom(selectedRoom);
+    } else {
+      alert("Du har inte valt ett rum!");
+    }
+  };
+
   return (
     <>
-    {!leaveChat ? (
-      <div className="chat-container">
-    <header className="chat-header">
-      <h1><i className="fas fa-smile"></i> ChatPage</h1>
-    </header>
-    <main className="chat-main">
-      <div className="chat-sidebar">
-        <h3><i className="fas fa-comments"></i> Room Name:</h3>
-        <h2 id="room-name">
-          <p>{currentRoom}</p> 
-          <label>Select</label>
-          <select>
-            {roomList.map((roomName, idx) => (
-              <option value={roomName} key={idx}>{roomName}
-              </option>
-            ))}
-          </select>
-          <button>Gå in i rummet</button> <br />
-          <button onClick={leaveRoom}>Leave room</button>
-        <input
-            type="text"
-            value={newRoom}
-            onChange={(e) => setNewroom(e.target.value)}
+      {!leaveChat ? (
+        <div className="chat-container">
+          <header className="chat-header">
+            <h1>
+              <i className="fas fa-smile"></i> ChatPage
+            </h1>
+          </header>
+          <main className="chat-main">
+            <div className="chat-sidebar">
+              <h3>
+                <i className="fas fa-comments"></i> Room Name:
+              </h3>
+              <h2 id="room-name">
+                <p>{currentRoom}</p>
+                <label>Select</label>
+                <select
+                  value={selectedRoom}
+                  onChange={(e) => setSelectedRoom(e.target.value)}
+                >
+                  {roomList.map((roomName, idx) => (
+                    <option value={roomName} key={idx}>
+                      {roomName}
+                    </option>
+                  ))}
+                </select>
+                <button onClick={joinSelectedRoom}>Gå in i rummet</button>{" "}
+                <br />
+                <button onClick={leaveRoom}>Lämna rum</button>
+                <input
+                  type="text"
+                  value={newRoom}
+                  onChange={(e) => setNewroom(e.target.value)}
+                />
+                <button onClick={checkRoomInput}>Skapa rum</button>
+              </h2>
+              <h3>
+                <i className="fas fa-users"></i> User:
+              </h3>
+              <ul id="users">
+                <li>{newUsername}</li>
+              </ul>
+            </div>
+            <div className="chat-messages">
+              {/* Här borde meddelande skrivas ut */}
+              {messageList.map((messageContent, idx) => (
+                <p key={idx}>
+                  klockan {messageContent.time} skrev {messageContent.author}:{" "}
+                  {messageContent.msg}
+                </p>
+              ))}
+            </div>
+          </main>
+          <div className="chat-form-container">
+            <input
+              id="msg"
+              type="text"
+              placeholder="Enter Message"
+              onChange={(e) => setCurrentMessage(e.target.value)}
+              required
             />
-          
-          <button onClick={checkRoomInput}>Skapa rum</button>
-          </h2>
-        <h3><i className="fas fa-users"></i> User:</h3>
-        <ul id="users">
-          <li>{newUsername}</li>
-        </ul>
-      </div>
-      <div className="chat-messages">
-        {/* Här borde meddelande skrivas ut */}
-        {messageList.map((messageContent, idx) => (
-          <p key={idx}>klockan {messageContent.time} skrev {messageContent.author}: {messageContent.msg}</p>
-          ))}
-      </div>
-    </main>
-    <div className="chat-form-container">
-      
-        <input
-          id="msg"
-          type="text"
-          placeholder="Enter Message"
-          onChange={(e) => setCurrentMessage(e.target.value)}
-          required
-          />
-        <button onClick={sendMessage} className="btn">Send</button>
-      
-    </div>
-    </div>
-    ) : (
-      <Users />
-    )}
-    
+            <button onClick={sendMessage} className="btn">
+              Send
+            </button>
+          </div>
+        </div>
+      ) : (
+        <Users />
+      )}
     </>
-    );
-  };
-  
-  export default ChatPage;
+  );
+};
+
+export default ChatPage;
